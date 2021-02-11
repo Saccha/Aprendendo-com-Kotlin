b39711d27cbde65a42ce6c4968532d576ebb4c67
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-02-11 09:56:37 -0300

    Create TiposdeDados.kt

---
+++ TiposdeDados.kt
@@ -0,0 +1,16 @@
+/*
+ -Int     -Boolean
+ -Long    -Char
+ -Float   -Byte
+ -Double  -Short
+ -Array   -Null!
+ 
+ */
+ 
+ /*Examples*/
+ 
+ println(Int.MAX_VALUE)
+ println(Float.MAX_VALUE)
+ println(Long.MAX_VALUE)
+ println(Byte.MAX_VALUE)
+ println(Short.MAX_VALUE)
