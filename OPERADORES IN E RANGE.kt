8875d2f4d38048a2329f063dcd0d0e423d029e7e
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-02-11 11:49:53 -0300

    Create OPERADORES IN E RANGE.kt

---
+++ OPERADORES IN E RANGE.kt
@@ -0,0 +1,16 @@
+/*
+Se valor está presente em uma lista ou uma faixa(range) de valores;
+Range cria um intervalo de valores que inicia no primeiro parâmetro e acaba no segundo 
+
+-CONTÉM(IN)
+-NÃO CONTÉM (!IN)
+-RANGE/FAIXA DE VALORES(INT..INT)
+
+Examples
+
+val numbers = listOf(3,9,0,1,2)
+print(12 in numbers)
+//false
+print(12 in 0..20)
+//true
+*/
