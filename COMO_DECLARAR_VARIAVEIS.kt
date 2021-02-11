387be09fef7c6bafbc27b08db33738d769af6bf2
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-02-11 10:03:08 -0300

    Create COMO_DECLARAR_VARIAVEIS.kt

---
+++ COMO_DECLARAR_VARIAVEIS.kt
@@ -0,0 +1,27 @@
+/*
+Var(valor mutável,CamelCase):
+  Variável que pode ter valor alterado durante o código;
+  Example:
+*/
+var currentAge = 22
+var currentAge:Int?
+currentAge = null ou 22
+
+/*
+Val(valor imutável, CamelCase):
+  Variável que terá somente o valor atribuído
+  (similar ao final em Java);
+  Example:
+*/
+val currentAge = 22
+val currentAge:Int?
+currentAge = null ou 22
+
+/*
+Const Val(valor imutável, SNAKE_CASE):
+  Constante cujo valor é atribuído durante compilação
+  Example:
+*/
+
+const val MIN_AGE = 16
+const val MAX_AGE = 68
