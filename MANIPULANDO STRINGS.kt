03abdce74f0a26bdcbfb69f612b07bc15ee64081
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-02-11 13:09:28 -0300

    Create MANIPULANDO STRINGS.kt

---
+++ MANIPULANDO STRINGS.kt
@@ -0,0 +1,10 @@
+/*
+Strings possuem diversos métodos associados;
+indexação, concatenação, comparação,formatação;
+Pode ser concatenada com plus/+;
+Também é tratado com um array de char;
+Para concatenar uma variável a uma String, os súmbolos ${} deveme ser inseridos
+
+Examples*/
+
+
