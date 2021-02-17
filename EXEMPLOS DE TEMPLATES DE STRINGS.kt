d930cfe9a11c94c90df933e78504d75c06b3fea9
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-02-17 00:09:20 -0300

    Create EXEMPLOS DE TEMPLATES DE STRINGS.kt

---
+++ EXEMPLOS DE TEMPLATES DE STRINGS.kt
@@ -0,0 +1,12 @@
+//Exemplo 1
+fun main(args: Array<String>){
+ 	val aprovados = listOf("João" , "Maria" , "Pedro")
+    print("O primeiro colocado foi ${aprovados[0]}.")
+}
+
+//Exemplo 2
+fun main(args: Array<String>){
+ 	val bomHumor = true
+    print("Hoje estou ${if (bomHumor) "feliz" else "Chateado"}.")
+}
+
