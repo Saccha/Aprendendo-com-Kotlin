a8c4c3a1ffb219bbd683f3ef7343e7d43b12b868
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-02-17 23:20:22 -0300

    Update EXEMPLO DE IF E ELSE.kt

--- EXEMPLO DE IF E ELSE.kt
+++ EXEMPLO DE IF E ELSE.kt
@@ -3,10 +3,24 @@
 var temperatura = 12
 
 fun main(){
-	print("Questão 1 Resposta\n")
-	if (temperatura >= 21) {
-    	print("Pode vestir roupas leves hoje\n")
-	} else {
-    	print("Melhor levar um casaco\n")
-	}
+   if (temperatura >= 21) {
+   print("Pode vestir roupas leves hoje\n")
+   } else {
+    print("Melhor levar um casaco\n")
+   }
 }
+
+/*Exemplo 2*/
+fun main(){
+   if temperatura >= 21 {
+      print("Pode vestir roupas leves hoje\n")
+   } else if (temperatura <= 12){
+      print("Você precisa mesmo sair de casa? Está muito frio lá fora!\n")
+   }else {
+      print("Melhor levar um casaco\n")
+   }
+}
+
+
+
+
