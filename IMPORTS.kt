4f99c56ea707f911d56843b766b3ef5a7300c92a
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-02-17 22:36:45 -0300

    Create IMPORTS.kt

---
+++ IMPORTS.kt
@@ -0,0 +1,23 @@
+/*
+    kotlin
+    kotlin.annotation
+    kotlin.collections
+    kotlin.comparisons
+    kotlin.io
+    kotlin.ranges
+    kotlin.sequences
+    kotlin.text
+    kotlin.math
+*/
+
+/*
+
+
+Importing certain entities may be disallowed by their visibility modifiers.
+
+    public entities can be imported anywhere
+    internal entities can be imported only within the same module
+    protected entities cannot be imported
+    top-level private entities can be imported within their declaring file
+    other private entities cannot be imported
+*/
