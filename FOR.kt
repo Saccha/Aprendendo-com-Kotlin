5276b789c428c85bff0ca70edfc50ed2b23a3b91
Saccharomyces <62216740+Saccha@users.noreply.github.com>
2021-02-18 09:56:18 -0300

    Update FOR.kt

--- FOR.kt
+++ FOR.kt
@@ -23,3 +23,11 @@ fun main(args: Array<String>) {
   println(i)
   }
 }
+
+/*Itera com Acesso ao Índice*/
+fun main(args: Array<String>) {
+  val alunos = arrayListOf("André", "Carla", "Marcos")
+  for ((indice, aluno) in alunos.withIndex()) {
+  println("$indice - $aluno \n" )
+  }
+}
